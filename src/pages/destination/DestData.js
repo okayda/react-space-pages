442ca97ecc0d50c418023615f799a5ceaf0efb1c
Jhon <59811681+okayda@users.noreply.github.com>
2023-01-29 07:42:38 -0800

    Add content data at destination

--- src/pages/destination/DestData.js
+++ src/pages/destination/DestData.js
@@ -1,3 +1,4 @@
+// destination images
 export const DestImgs = [
   {
     id: "moon",
@@ -7,16 +8,112 @@ export const DestImgs = [
   {
     id: "mars",
     url: "image-mars.webp",
-    alt: "Moon",
+    alt: "Mars",
   },
   {
     id: "europa",
     url: "image-europa.webp",
-    alt: "Moon",
+    alt: "Europa",
   },
   {
     id: "titan",
     url: "image-titan.webp",
-    alt: "Moon",
+    alt: "Titan",
+  },
+];
+
+// destination description
+export const DestDesc = [
+  {
+    id: "moon",
+    title: "Moon",
+    description: `See our planet as you’ve never seen it before. A perfect relaxing trip
+    away to help regain perspective and come back refreshed. While you’re
+    there, take in some history by visiting the Luna 2 and Apollo 11 landing
+    sites.`,
+  },
+  {
+    id: "mars",
+    title: "Mars",
+    description: `Don’t forget to pack your hiking boots. You’ll need them to tackle
+    Olympus Mons, the tallest planetary mountain in our solar system. It’s
+    two and a half times the size of Everest!`,
+  },
+  {
+    id: "europa",
+    title: "Europa",
+    description: `The smallest of the four Galilean moons orbiting Jupiter, Europa is a
+    winter lover’s dream. With an icy surface, it’s perfect for a bit of ice
+    skating, curling, hockey, or simple relaxation in your snug wintery
+    cabin.`,
+  },
+  {
+    id: "titan",
+    title: "Titan",
+    description: `The smallest of the four Galilean moons orbiting Jupiter, Europa is a
+    winter lover’s dream. With an icy surface, it’s perfect for a bit of ice
+    skating, curling, hockey, or simple relaxation in your snug wintery
+    cabin.`,
+  },
+];
+
+//destination information
+const DestInfo = [
+  {
+    id: "moon",
+    data: [
+      {
+        point: "avg. distance",
+        calculation: "384,400 km",
+      },
+
+      {
+        point: "est. travel time",
+        calculation: "3 days",
+      },
+    ],
+  },
+
+  {
+    id: "mars",
+    data: [
+      {
+        point: "avg. distance",
+        calculation: "225 MIL. km",
+      },
+
+      {
+        point: "est. travel time",
+        calculation: "9 months",
+      },
+    ],
+  },
+  {
+    id: "europa",
+    data: [
+      {
+        point: "avg. distance",
+        calculation: "628 MIL. km",
+      },
+
+      {
+        point: "est. travel time",
+        calculation: "3 years",
+      },
+    ],
+  },
+  {
+    id: "titan",
+    data: [
+      {
+        point: "avg. distance",
+        calculation: "1.6 BIL. km",
+      },
+
+      {
+        point: "est. travel time",
+        calculation: "7 years",
+      },
+    ],
   },
 ];
